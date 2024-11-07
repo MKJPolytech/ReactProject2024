493912c44d8c4c88fbb1b55b149be8eaa5d1df26
MKJPolytech <mkjpolytech@daum.net>
2024-11-07 14:29:25 +0900

    react 9장 조건부렌더링, 엘리먼트 변수

--- reactproject/src/index.js
+++ reactproject/src/index.js
@@ -5,6 +5,7 @@ import './index.css';
 // import './chp04/Clock.css'
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Book from "./chp03/Book";
 import Library from "./chp03/Library";
 import ConfirmDialog from "./chp04/ConfirmDialog";
 import Button from "./chp04/Button";
@@ -12,7 +13,7 @@ import Clock from "./chp04/Clock";
 
 import CommentList from "./chp05/CommentList";
 import NotificationList from "./chp06/NotificationList";
-// import Accommodate from "./chp07/Accommodate";
+import Accommodate from "./chp07/Accommodate";
 import Toggle from "./chp08/Toggle"
 import MyButton from "./chp08/MyButton";
 import Toggle2 from "./chp08/Toggle2";
@@ -23,6 +24,7 @@ import InputTest from "./chp08/InputTest";
 import ConfirmButton3 from "./chp08/ConfirmButton3";
 import ConfirmButton from "./chp08/ConfirmButton";
 import ConfirmButton2 from "./chp08/ConfirmButton2";
+import LoginControl from "./chp09/LoginControl";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -38,7 +40,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // )
 root.render(
   <React.StrictMode>
-    <ConfirmButton/>
+    <LoginControl/>
   </React.StrictMode>
 );
 
