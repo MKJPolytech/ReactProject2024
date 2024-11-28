6af6ed7b35164bce053ee009d2e14f7f62526ad0
MKJPolytech <mkjpolytech@daum.net>
2024-11-28 16:32:10 +0900

    react 12장 (2024-11-2) 부모컴포넌트 상태변수를 하위 컴포넌트에서 공유하는 방법

--- reactproject/src/index.js
+++ reactproject/src/index.js
@@ -37,6 +37,10 @@ import NameForm from "./chp11/NameForm";
 import RequestForm from "./chp11/RequestForm";
 import NameRequestForm from "./chp11/NameRequestForm";
 import SignUp from "./chp11/SignUp";
+import Calculator from "./chp12/Calculator";
+import Calculator2 from "./chp12/Calculator2";
+import Calculator3 from "./chp12/Calculator3";
+import DistanceConverter from "./chp12/distance/DistanceConverter";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 // setInterval(() =>
@@ -51,7 +55,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 // )
 root.render(
   <React.StrictMode>
-    <SignUp/>
+    <DistanceConverter/>
   </React.StrictMode>
 );
 
